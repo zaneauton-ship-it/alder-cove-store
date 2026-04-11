124b3b79392e7d1e715ed5e7c61cddda4a46b257
zaneauton-ship-it <zaneauton@icloud.com>
2026-04-11 13:00:19 -0400

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+window.STORE_CONFIG = {
+  siteUrl: "https://www.foothillslistings.online",
+  stripePublishableKey: "pk_live_51TJj9bKYxk8NB6DIXZCgtbK43DoBsVDY9xfiLNe0lS1cKXN2Ifw6pEoSfH4Q8AQz6mr7G2fzUKaD3EJ6HCOTprMx00eo2YtKCy",
+  supportEmail: "info@foothillslistings.online"
+};
