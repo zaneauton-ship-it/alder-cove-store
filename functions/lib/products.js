dd1905bbf2bbd4cba63ad30e854b9a8eaa67cf28
zaneauton-ship-it <zaneauton@icloud.com>
2026-04-11 13:06:26 -0400

    Add files via upload

---
+++ functions/lib/products.js
@@ -0,0 +1,442 @@
+export const PRODUCTS = {
+  "W005319152": {
+    "name": "Lunette Single Bathroom Vanity Set",
+    "price": 869.99,
+    "image": "assets/placeholders/lunette-single-bathroom-vanity-set-w005319152.svg"
+  },
+  "W002843555": {
+    "name": "Rivers 18\" Single Bathroom Vanity with Marble Top",
+    "price": 749.0,
+    "image": "assets/placeholders/rivers-18-single-bathroom-vanity-with-marble-top-w002843555.svg"
+  },
+  "W004901187": {
+    "name": "Rivers 21\" Single Bathroom Vanity with Marble Top",
+    "price": 849.0,
+    "image": "assets/placeholders/rivers-21-single-bathroom-vanity-with-marble-top-w004901187.svg"
+  },
+  "W113326805": {
+    "name": "Breonica 24\" Mid Century Modern Single Bathroom Vanity with Ceramic Sink Top, Zinc Alloy Faucet, Painted Surface",
+    "price": 336.98,
+    "image": "assets/placeholders/breonica-24-mid-century-modern-single-bathroom-vanity-with-ceramic-sink-top-zinc-alloy-faucet-painted-surface-w113326805.svg"
+  },
+  "W005405071": {
+    "name": "Caitlyn 24\" Single Bathroom Vanity with Ceramic Top",
+    "price": 399.0,
+    "image": "assets/placeholders/caitlyn-24-single-bathroom-vanity-with-ceramic-top-w005405071.svg"
+  },
+  "VLHV1072": {
+    "name": "Arlowene 24\" Single Bathroom Vanity with Ceramic Top",
+    "price": 409.99,
+    "image": "assets/placeholders/arlowene-24-single-bathroom-vanity-with-ceramic-top-vlhv1072.svg"
+  },
+  "SGRV1507": {
+    "name": "Vicini 24\" Single Vanity with Ceramic Top",
+    "price": 549.99,
+    "image": "assets/placeholders/vicini-24-single-vanity-with-ceramic-top-sgrv1507.svg"
+  },
+  "W004216993": {
+    "name": "Lexington 24\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 579.99,
+    "image": "assets/placeholders/lexington-24-single-bathroom-vanity-with-engineered-marble-top-w004216993.svg"
+  },
+  "W008006767": {
+    "name": "Slayton 24\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 629.0,
+    "image": "assets/placeholders/slayton-24-single-bathroom-vanity-with-engineered-marble-top-w008006767.svg"
+  },
+  "W004414415": {
+    "name": "Hermes 24\" Single Bathroom Vanity with Marble Top",
+    "price": 699.0,
+    "image": "assets/placeholders/hermes-24-single-bathroom-vanity-with-marble-top-w004414415.svg"
+  },
+  "W009149897": {
+    "name": "Angelissa 24\" Single Bathroom Vanity Set",
+    "price": 819.99,
+    "image": "assets/placeholders/angelissa-24-single-bathroom-vanity-set-w009149897.svg"
+  },
+  "W000549021": {
+    "name": "Rivers 24\" Single Bathroom Vanity with Marble Top",
+    "price": 849.0,
+    "image": "assets/placeholders/rivers-24-single-bathroom-vanity-with-marble-top-w000549021.svg"
+  },
+  "W004725104": {
+    "name": "Jewell 30\" Single Bathroom Vanity with Ceramic Top",
+    "price": 499.99,
+    "image": "assets/placeholders/jewell-30-single-bathroom-vanity-with-ceramic-top-w004725104.svg"
+  },
+  "W004929985": {
+    "name": "Selma 30\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 569.99,
+    "image": "assets/placeholders/selma-30-single-bathroom-vanity-with-engineered-marble-top-w004929985.svg"
+  },
+  "W002812267": {
+    "name": "Soma 30\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 589.99,
+    "image": "assets/placeholders/soma-30-single-bathroom-vanity-with-engineered-marble-top-w002812267.svg"
+  },
+  "W011221106": {
+    "name": "Gavino 30\" Single Bathroom Vanity with Stone Top",
+    "price": 799.99,
+    "image": "assets/placeholders/gavino-30-single-bathroom-vanity-with-stone-top-w011221106.svg"
+  },
+  "W005666806": {
+    "name": "Cayden 30\" Single Bathroom Vanity with Top",
+    "price": 829.99,
+    "image": "assets/placeholders/cayden-30-single-bathroom-vanity-with-top-w005666806.svg"
+  },
+  "W111714666": {
+    "name": "Trayshawn 30 in. Whitebrook Quartz Single Countertop Vanity",
+    "price": 846.37,
+    "image": "assets/placeholders/trayshawn-30-in-whitebrook-quartz-single-countertop-vanity-w111714666.svg"
+  },
+  "W004385225": {
+    "name": "Felicity 30\" Single Bathroom Vanity with Stone Top",
+    "price": 849.0,
+    "image": "assets/placeholders/felicity-30-single-bathroom-vanity-with-stone-top-w004385225.svg"
+  },
+  "W008006991": {
+    "name": "Slayton 30\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 899.0,
+    "image": "assets/placeholders/slayton-30-single-bathroom-vanity-with-engineered-marble-top-w008006991.svg"
+  },
+  "FRNY2913": {
+    "name": "Pelaghia 30\" Single Bathroom Vanity with Marble Top",
+    "price": 899.0,
+    "image": "assets/placeholders/pelaghia-30-single-bathroom-vanity-with-marble-top-frny2913.svg"
+  },
+  "W005405130": {
+    "name": "Minnetrista Single Bathroom Vanity with Marble Top",
+    "price": 899.99,
+    "image": "assets/placeholders/minnetrista-single-bathroom-vanity-with-marble-top-w005405130.svg"
+  },
+  "W001501630": {
+    "name": "Rivers 30\" Single Bathroom Vanity with Marble Top",
+    "price": 1049.0,
+    "image": "assets/placeholders/rivers-30-single-bathroom-vanity-with-marble-top-w001501630.svg"
+  },
+  "W008006852": {
+    "name": "Soma 32\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 799.99,
+    "image": "assets/placeholders/soma-32-single-bathroom-vanity-with-engineered-marble-top-w008006852.svg"
+  },
+  "W000865027": {
+    "name": "Hermes 32\" Single Bathroom Vanity with Marble Top",
+    "price": 949.0,
+    "image": "assets/placeholders/hermes-32-single-bathroom-vanity-with-marble-top-w000865027.svg"
+  },
+  "W007574590": {
+    "name": "Rivers 32\" Single Bathroom Vanity with Marble Top",
+    "price": 1099.0,
+    "image": "assets/placeholders/rivers-32-single-bathroom-vanity-with-marble-top-w007574590.svg"
+  },
+  "W002809349": {
+    "name": "Krish 36\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 819.99,
+    "image": "assets/placeholders/krish-36-single-bathroom-vanity-with-engineered-marble-top-w002809349.svg"
+  },
+  "W002809436": {
+    "name": "Irene 36\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 869.99,
+    "image": "assets/placeholders/irene-36-single-bathroom-vanity-with-engineered-marble-top-w002809436.svg"
+  },
+  "W004003571": {
+    "name": "Ferdinand 36\" Single Bathroom Vanity with Top",
+    "price": 999.0,
+    "image": "assets/placeholders/ferdinand-36-single-bathroom-vanity-with-top-w004003571.svg"
+  },
+  "W005667082": {
+    "name": "Radstock 36\" Single Bathroom Vanity with Top",
+    "price": 1019.99,
+    "image": "assets/placeholders/radstock-36-single-bathroom-vanity-with-top-w005667082.svg"
+  },
+  "W003260196": {
+    "name": "Fossen 36\" Single Bathroom Vanity with Top",
+    "price": 1049.99,
+    "image": "assets/placeholders/fossen-36-single-bathroom-vanity-with-top-w003260196.svg"
+  },
+  "CDCB1052": {
+    "name": "Steve 36\" Single Bathroom Vanity with Marble Top",
+    "price": 1052.32,
+    "image": "assets/placeholders/steve-36-single-bathroom-vanity-with-marble-top-cdcb1052.svg"
+  },
+  "W011221153": {
+    "name": "Gavino 36\" Single Bathroom Vanity with Stone Top",
+    "price": 1129.99,
+    "image": "assets/placeholders/gavino-36-single-bathroom-vanity-with-stone-top-w011221153.svg"
+  },
+  "W004415926": {
+    "name": "Hermes 36\" Single Bathroom Vanity with Marble Top",
+    "price": 1149.0,
+    "image": "assets/placeholders/hermes-36-single-bathroom-vanity-with-marble-top-w004415926.svg"
+  },
+  "W002726667": {
+    "name": "Rivers 36\" Single Bathroom Vanity with Marble Top",
+    "price": 1299.0,
+    "image": "assets/placeholders/rivers-36-single-bathroom-vanity-with-marble-top-w002726667.svg"
+  },
+  "W001788934": {
+    "name": "Rivers 40\" Single Bathroom Vanity",
+    "price": 1399.0,
+    "image": "assets/placeholders/rivers-40-single-bathroom-vanity-w001788934.svg"
+  },
+  "FRNY4252": {
+    "name": "Rivers 40\" Single Bathroom Vanity with Carrara Marble Top",
+    "price": 1399.0,
+    "image": "assets/placeholders/rivers-40-single-bathroom-vanity-with-carrara-marble-top-frny4252.svg"
+  },
+  "W008007599": {
+    "name": "Soma 42\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 949.99,
+    "image": "assets/placeholders/soma-42-single-bathroom-vanity-with-engineered-marble-top-w008007599.svg"
+  },
+  "W002809401": {
+    "name": "Irene 42\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 979.99,
+    "image": "assets/placeholders/irene-42-single-bathroom-vanity-with-engineered-marble-top-w002809401.svg"
+  },
+  "W005509765": {
+    "name": "Jillian 42\" Free-standing Single Bathroom Vanity with Engineered Stone Vanity Top",
+    "price": 1029.99,
+    "image": "assets/placeholders/jillian-42-free-standing-single-bathroom-vanity-with-engineered-stone-vanity-top-w005509765.svg"
+  },
+  "W009782013": {
+    "name": "Ajhane 42\" Single Bathroom Vanity Set",
+    "price": 1049.93,
+    "image": "assets/placeholders/ajhane-42-single-bathroom-vanity-set-w009782013.svg"
+  },
+  "W004217102": {
+    "name": "Lexington 42\" Single Bathroom Vanity with Top",
+    "price": 1069.99,
+    "image": "assets/placeholders/lexington-42-single-bathroom-vanity-with-top-w004217102.svg"
+  },
+  "W110601251": {
+    "name": "Stratford 42\" Single Bathroom Vanity with Engineered Stone Top",
+    "price": 1099.99,
+    "image": "assets/placeholders/stratford-42-single-bathroom-vanity-with-engineered-stone-top-w110601251.svg"
+  },
+  "W111409486": {
+    "name": "Bilroy Egremt 42\" Single Bathroom Vanity with Engineered Stone Top",
+    "price": 1128.0,
+    "image": "assets/placeholders/bilroy-egremt-42-single-bathroom-vanity-with-engineered-stone-top-w111409486.svg"
+  },
+  "W005666604": {
+    "name": "Radstock 42\" Single Bathroom Vanity with Top",
+    "price": 1239.99,
+    "image": "assets/placeholders/radstock-42-single-bathroom-vanity-with-top-w005666604.svg"
+  },
+  "W003205959": {
+    "name": "Currahee 42\" Single Bathroom Vanity Set",
+    "price": 1266.45,
+    "image": "assets/placeholders/currahee-42-single-bathroom-vanity-set-w003205959.svg"
+  },
+  "W007164534": {
+    "name": "Aisa 42\" Single Bathroom Vanity",
+    "price": 1349.0,
+    "image": "assets/placeholders/aisa-42-single-bathroom-vanity-w007164534.svg"
+  },
+  "W004497891": {
+    "name": "Hermes 42\" Single Bathroom Vanity Set",
+    "price": 1449.0,
+    "image": "assets/placeholders/hermes-42-single-bathroom-vanity-set-w004497891.svg"
+  },
+  "W004906813": {
+    "name": "Rivers 42\" Single Bathroom Vanity with Marble Top",
+    "price": 1699.0,
+    "image": "assets/placeholders/rivers-42-single-bathroom-vanity-with-marble-top-w004906813.svg"
+  },
+  "W008088046": {
+    "name": "Eugene 48\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 649.99,
+    "image": "assets/placeholders/eugene-48-single-bathroom-vanity-with-engineered-marble-top-w008088046.svg"
+  },
+  "W004810937": {
+    "name": "Amyra 48\" Single Bathroom Vanity with Marble Top",
+    "price": 779.99,
+    "image": "assets/placeholders/amyra-48-single-bathroom-vanity-with-marble-top-w004810937.svg"
+  },
+  "W009698139": {
+    "name": "Binne 48\" Single Bathroom Vanity with Top",
+    "price": 999.99,
+    "image": "assets/placeholders/binne-48-single-bathroom-vanity-with-top-w009698139.svg"
+  },
+  "W002809347": {
+    "name": "Krish 48\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 1139.99,
+    "image": "assets/placeholders/krish-48-single-bathroom-vanity-with-engineered-marble-top-w002809347.svg"
+  },
+  "W004343145": {
+    "name": "Ferdinand 48\" Single Bathroom Vanity with Top",
+    "price": 1139.99,
+    "image": "assets/placeholders/ferdinand-48-single-bathroom-vanity-with-top-w004343145.svg"
+  },
+  "W004725783": {
+    "name": "Jillian 48\" Single Bathroom Vanity with Engineered Stone Top",
+    "price": 1159.99,
+    "image": "assets/placeholders/jillian-48-single-bathroom-vanity-with-engineered-stone-top-w004725783.svg"
+  },
+  "W005666712": {
+    "name": "Radstock 48\" Single Bathroom Vanity with Top",
+    "price": 1249.99,
+    "image": "assets/placeholders/radstock-48-single-bathroom-vanity-with-top-w005666712.svg"
+  },
+  "W001676732": {
+    "name": "Lexington 48\" Single Bathroom Vanity with Marble Top",
+    "price": 1279.99,
+    "image": "assets/placeholders/lexington-48-single-bathroom-vanity-with-marble-top-w001676732.svg"
+  },
+  "SGRV1529": {
+    "name": "Fairuza 48\" Single Vanity",
+    "price": 1289.99,
+    "image": "assets/placeholders/fairuza-48-single-vanity-sgrv1529.svg"
+  },
+  "W004040678": {
+    "name": "Freitas 48\" Solid Wood Single Bathroom Vanity with 1.5\" Quartz Top",
+    "price": 1332.7,
+    "image": "assets/placeholders/freitas-48-solid-wood-single-bathroom-vanity-with-1-5-quartz-top-w004040678.svg"
+  },
+  "W009360679": {
+    "name": "Ajiya 48\" Single Bathroom Vanity",
+    "price": 1419.99,
+    "image": "assets/placeholders/ajiya-48-single-bathroom-vanity-w009360679.svg"
+  },
+  "W011221211": {
+    "name": "Gavino 48\" Single Bathroom Vanity with Stone Top",
+    "price": 1429.99,
+    "image": "assets/placeholders/gavino-48-single-bathroom-vanity-with-stone-top-w011221211.svg"
+  },
+  "W009632254": {
+    "name": "Ajhane 48\" Single Vanity",
+    "price": 1444.65,
+    "image": "assets/placeholders/ajhane-48-single-vanity-w009632254.svg"
+  },
+  "W004497868": {
+    "name": "Hermes 48\" Single Bathroom Vanity with Marble Top",
+    "price": 1499.0,
+    "image": "assets/placeholders/hermes-48-single-bathroom-vanity-with-marble-top-w004497868.svg"
+  },
+  "W008322512": {
+    "name": "Curiel 48\" Single Bathroom Vanity",
+    "price": 1699.99,
+    "image": "assets/placeholders/curiel-48-single-bathroom-vanity-w008322512.svg"
+  },
+  "W110601249": {
+    "name": "Stratford 54\" Double Bathroom Vanity with Engineered Stone Top",
+    "price": 1389.99,
+    "image": "assets/placeholders/stratford-54-double-bathroom-vanity-with-engineered-stone-top-w110601249.svg"
+  },
+  "W110274986": {
+    "name": "Soma 54\" Single Bathroom Vanity with Engineered Marble Top",
+    "price": 1479.99,
+    "image": "assets/placeholders/soma-54-single-bathroom-vanity-with-engineered-marble-top-w110274986.svg"
+  },
+  "W007207073": {
+    "name": "Vicini 54\" Double Bathroom Vanity Set",
+    "price": 1569.99,
+    "image": "assets/placeholders/vicini-54-double-bathroom-vanity-set-w007207073.svg"
+  },
+  "ADRL1866": {
+    "name": "Chimira 60\" Double Sink Bathroom Vanity With Sink Combo Set With 2 Drawers & Doors",
+    "price": 590.0,
+    "image": "assets/placeholders/chimira-60-double-sink-bathroom-vanity-with-sink-combo-set-with-2-drawers-doors-adrl1866.svg"
+  },
+  "W001746985": {
+    "name": "Braylen 60\" Double Bathroom Vanity with Vitreous China Top",
+    "price": 699.99,
+    "image": "assets/placeholders/braylen-60-double-bathroom-vanity-with-vitreous-china-top-w001746985.svg"
+  },
+  "W009698257": {
+    "name": "Binne 60\" Double Bathroom Vanity with Top",
+    "price": 1089.99,
+    "image": "assets/placeholders/binne-60-double-bathroom-vanity-with-top-w009698257.svg"
+  },
+  "W009698207": {
+    "name": "Binne 60\" Single Bathroom Vanity Set",
+    "price": 1199.99,
+    "image": "assets/placeholders/binne-60-single-bathroom-vanity-set-w009698207.svg"
+  },
+  "W005511266": {
+    "name": "Jillian 60\" Single Bathroom Vanity with Engineered Stone Top",
+    "price": 1199.99,
+    "image": "assets/placeholders/jillian-60-single-bathroom-vanity-with-engineered-stone-top-w005511266.svg"
+  },
+  "W005485186": {
+    "name": "Larosa 60\" Double Bathroom Vanity with Top",
+    "price": 1389.99,
+    "image": "assets/placeholders/larosa-60-double-bathroom-vanity-with-top-w005485186.svg"
+  },
+  "W011221298": {
+    "name": "Gavino 60\" Double Bathroom Vanity with Engineered Stone Top",
+    "price": 1439.99,
+    "image": "assets/placeholders/gavino-60-double-bathroom-vanity-with-engineered-stone-top-w011221298.svg"
+  },
+  "W003260152": {
+    "name": "Fossen 60\" Single Bathroom Vanity with Top",
+    "price": 1449.99,
+    "image": "assets/placeholders/fossen-60-single-bathroom-vanity-with-top-w003260152.svg"
+  },
+  "SGRV1531": {
+    "name": "Fairuza 60\" Double Vanity",
+    "price": 1449.99,
+    "image": "assets/placeholders/fairuza-60-double-vanity-sgrv1531.svg"
+  },
+  "W110601247": {
+    "name": "Stratford 60\" Single Bathroom Vanity with Engineered Stone Top",
+    "price": 1489.99,
+    "image": "assets/placeholders/stratford-60-single-bathroom-vanity-with-engineered-stone-top-w110601247.svg"
+  },
+  "W004389333": {
+    "name": "Ferdinand 60\" Single Bathroom Vanity with Top",
+    "price": 1549.0,
+    "image": "assets/placeholders/ferdinand-60-single-bathroom-vanity-with-top-w004389333.svg"
+  },
+  "W007194470": {
+    "name": "Vicini 60\" Single Bathroom Vanity Set",
+    "price": 1569.99,
+    "image": "assets/placeholders/vicini-60-single-bathroom-vanity-set-w007194470.svg"
+  },
+  "W009624685": {
+    "name": "Ajhane 60\" Double Vanity",
+    "price": 1697.4,
+    "image": "assets/placeholders/ajhane-60-double-vanity-w009624685.svg"
+  },
+  "W011160684": {
+    "name": "Freitas 60\" Solid Wood Single Bathroom Vanity with 1.5\" Carrara Marble Top",
+    "price": 1791.0,
+    "image": "assets/placeholders/freitas-60-solid-wood-single-bathroom-vanity-with-1-5-carrara-marble-top-w011160684.svg"
+  },
+  "W005525245": {
+    "name": "Hermes 60\" Single Bathroom Vanity with Marble Top",
+    "price": 2099.0,
+    "image": "assets/placeholders/hermes-60-single-bathroom-vanity-with-marble-top-w005525245.svg"
+  },
+  "W005638674": {
+    "name": "Hermes 60\" Double Bathroom Vanity with Marble Top",
+    "price": 2199.0,
+    "image": "assets/placeholders/hermes-60-double-bathroom-vanity-with-marble-top-w005638674.svg"
+  },
+  "W110274989": {
+    "name": "Soma 72\" Double Bathroom Vanity with Engineered Marble Top",
+    "price": 1629.99,
+    "image": "assets/placeholders/soma-72-double-bathroom-vanity-with-engineered-marble-top-w110274989.svg"
+  },
+  "W100144165": {
+    "name": "Adela 72\" Double Bathroom Vanity with Engineered Stone Top",
+    "price": 2299.0,
+    "image": "assets/placeholders/adela-72-double-bathroom-vanity-with-engineered-stone-top-w100144165.svg"
+  },
+  "SGRV1516": {
+    "name": "Liljan 24\" Wall-Mounted Single Bathroom Vanity with Engineered Stone Top",
+    "price": 599.99,
+    "image": "assets/placeholders/liljan-24-wall-mounted-single-bathroom-vanity-with-engineered-stone-top-sgrv1516.svg"
+  },
+  "SGRV1514": {
+    "name": "Liljan 30\" Wall-Mounted Single Bathroom Vanity with Engineered Stone Top",
+    "price": 659.99,
+    "image": "assets/placeholders/liljan-30-wall-mounted-single-bathroom-vanity-with-engineered-stone-top-sgrv1514.svg"
+  },
+  "SGRV1515": {
+    "name": "Liljan 36\" Wall-Mounted Single Bathroom Vanity",
+    "price": 949.99,
+    "image": "assets/placeholders/liljan-36-wall-mounted-single-bathroom-vanity-sgrv1515.svg"
+  }
+};
